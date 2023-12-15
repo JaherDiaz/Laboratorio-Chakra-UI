89875e85f747c2bc262fd40d7071d91072038521
JaherDiaz <132585540+JaherDiaz@users.noreply.github.com>
2023-12-15 14:47:43 -0500

    .

---
+++ lab/src/components/Header.jsx
@@ -0,0 +1,38 @@
+import { Heading } from "@chakra-ui/react";
+import { Button, Stack, Text } from "@chakra-ui/react";
+import Card from "./Card";
+export default function Header() {
+  return (
+    <>
+      <Heading
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        marginTop="25px"
+        marginLeft="25px"
+        width="1020px"
+        height="360px"
+        textAlign="center"
+        variant="brand"
+        size="2xl"
+        fontWeight="hairline"
+        color="white"
+        fontSize="50px"
+        bgImage="https://momentummag.com/wp-content/uploads/2022/09/womansunset.jpg"
+      >
+        <Text marginBottom="15px" >DOMINA EL TERRENO</Text>
+
+        <Stack direction="row" spacing={4} align="center">
+          <Button fontWeight="low" borderRadius="0" colorScheme="gray" color="white" borderColor="white" _hover={{ bgColor: "white", color: "black" }} variant="outline" >
+            VER DETALLEs
+          </Button>
+          <Button fontWeight="low" borderRadius="0" colorScheme="gray" color="white" borderColor="white" _hover={{ bgColor: "white", color: "black" }} variant="outline">
+            VER VIDEO
+          </Button>
+        </Stack>
+      </Heading>
+      <Card/>
+    </>
+  );
+}
